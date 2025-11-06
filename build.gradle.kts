a2a94f836c01dcdaa368a3324be34f106ac1aaad
DouglasGrandi <105892030+DouglasGrandi@users.noreply.github.com>
2025-11-06 17:03:13 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
-
 plugins {
-    id("com.android.application") version "8.6.0" apply false
+    // Android Gradle Plugin (AGP) compatível com Gradle 8.7
+    id("com.android.application") version "8.5.2" apply false
+    // Kotlin 1.9.24
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
