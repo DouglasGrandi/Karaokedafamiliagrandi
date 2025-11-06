73eebfb90bde5c56dbfa993fa6962f5c2ff8978b
DouglasGrandi <105892030+DouglasGrandi@users.noreply.github.com>
2025-11-06 16:03:48 -0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,20 @@
+// settings.gradle.kts (raiz)
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
 
 rootProject.name = "Karaokedafamiliagrandi"
 include(":app")
