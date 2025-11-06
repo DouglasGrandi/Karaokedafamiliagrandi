f9fa64ea5c9d957e5cd26beeb11734dea3fc947d
DouglasGrandi <105892030+DouglasGrandi@users.noreply.github.com>
2025-11-06 16:56:56 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         compose = true
     }
     composeOptions {
-        { kotlinCompilerExtensionVersion = "1.5.11" }
+        { kotlinCompilerExtensionVersion = "1.5.14" }
     }
 
     // Sintaxe correta no Kotlin DSL
